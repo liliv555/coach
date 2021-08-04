28bc98f3d9135ad8ad33c4bb62d8a10aac001ec4
lili vincze <lili.vincze@gmail.com>
2021-08-04 14:09:20 +0200

    section structure

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,9 +9,28 @@ require("@rails/activestorage").start()
 require("channels")
 
 
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// $(document).ready(function() {
+//     $(window).on('scroll', function() {
+//      if($(window).scrollTop() < 1000) {
+//        $('.main-text').css('background-size', 130 + parseInt($(window).scrollTop() / 5) + '%');
+//     //    $('.main-text h1').css('top', 50 + ($(window).scrollTop() * .1) + '%');
+//        $('.main-text h1').css('opacity', 1 - ($(window).scrollTop() * .003));
+//      }
+      
+//       if($(window).scrollTop() >= $('.section').offset().top - 300) {
+//         $('.nav-bg').removeClass('bg-hidden');
+//         $('.nav-bg').addClass('bg-visible');
+//       } else {
+//         $('.nav-bg').removeClass('bg-visible');
+//         $('.nav-bg').addClass('bg-hidden');
+//       }
+//    });
+//  });
