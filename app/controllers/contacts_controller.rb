0c5aa4b5aa608e33b76b26949f1cf65552bf387f
lili vincze <lili.vincze@gmail.com>
2021-10-20 17:35:05 +0200

    trying to fix flash...

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,17 +2,16 @@ class ContactsController < ApplicationController
 
   def new
     @contact = Contact.new
-    @sent = false
   end
 
   def create
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      @sent = true
-      redirect_to root_path
+      flash[:success] = "All good!"
+      format.html { redirect_to root_path }
     else
-      flash[:error] = 'Could not send message'
+      flash.now[:error] = 'Could not send message'
       render :new
     end
   end
