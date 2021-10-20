f0f781e201c835f9c25be989cffae8b77780f778
lili vincze <lili.vincze@gmail.com>
2021-10-20 17:46:37 +0200

    changed :root_path to root_path

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      redirect_to :root_path, notice: "Message sent!"
+      redirect_to root_path, notice: "Message sent!"
       # flash.now[:success] = 'Message sent!'
       # redirect_to root_path
     else
